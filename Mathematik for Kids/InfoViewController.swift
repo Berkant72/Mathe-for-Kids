10ee735a032161f93049de031c62fa11f950a09b
Dursun Berkant <berkant.dursun@yahoo.de>
2016-01-18 23:55:30 +0100

    InfoViewController hinzugefügt

---
+++ Mathematik for Kids/InfoViewController.swift
@@ -0,0 +1,27 @@
+//
+//  InfoViewController.swift
+//  Mathematik for Kids
+//
+//  Created by Berkant Dursun on 18.01.16.
+//  Copyright © 2016 Berkant Dursun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class InfoViewController: UIViewController {
+    
+    @IBOutlet weak var infoLabel: UITextView!
+    
+    
+    override func viewDidLoad () {
+        infoLabel.text = "Das ist die Version 1.0."
+        
+        
+    }
+   
+    
+    
+}
+
